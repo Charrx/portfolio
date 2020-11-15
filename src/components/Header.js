033ce172e3f8fa6ead849c5b59ec32805d22e001
Lea Nygaard Sømod Jensen <lea.jensen@se.com>
2020-11-15 13:12:13 +0100

    App foundation

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export const Header = () => {
+    return(
+        <div className="header">
+            <h1>React With NodeJS</h1>
+            <FontAwesomeIcon icon="coffee" />
+        </div>
+    )
+}
